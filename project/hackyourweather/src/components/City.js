3b745d8351ff678ded6755c57890d7e5c5fc95b8
msnmz <snmez.muharrem@gmail.com>
2019-11-23 00:07:54 +0100

    Creates CityHeader component & style

--- project/hackyourweather/src/components/City.js
+++ project/hackyourweather/src/components/City.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "../styles/City.css";
+import CityHeader from "./CityHeader";
 
 function City(props) {
-  return <div className="city"></div>;
+  return (
+    <div className="city">
+      <CityHeader name={props.city.name} country={props.city.country} />
+    </div>
+  );
 }
 
 export default City;
