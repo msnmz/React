2db24ee33020a2efb0abce9e8bb62f0becaff3b6
msnmz <snmez.muharrem@gmail.com>
2019-12-15 02:18:51 +0100

    Finishes Project for week 4

--- project/hackyourweather/src/App.js
+++ project/hackyourweather/src/App.js
@@ -3,12 +3,15 @@ import Header from "./components/Header";
 import CityWeather from "./components/CityWeather/CityWeather";
 import Container from "@material-ui/core/Container";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Forecast from "./components/Forecast/Forecast";
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/:cityId'></Route>
+        <Route path='/:cityId'>
+          <Forecast />
+        </Route>
         <Route path='/'>
           <Container>
             <Header title='Weather' />
